bfdc8641a6e83eacba4d6bce96860dd71dcf66af
nicolas-fanjas-mercere <91787661+nicolas-fanjas-mercere@users.noreply.github.com>
2023-01-28 19:53:22 +0100

    Add files via upload

---
+++ password.py
@@ -0,0 +1,44 @@
+mdp = input("Bon mec pour créer un mot de passe il faut: 8 caractères, 1 Maj, 1 minuscule, 1 chiffre, 1 @ : ")
+
+def verif_taille_maxilmal(mdp):
+    if len(mdp) < 8:
+        return False
+    else:
+        return True
+
+def verif_minuscule(mdp):
+    for char in mdp:
+        if char.minuscule():
+            return True
+    return False
+
+def verif_majuscule(mdp):
+    for char in mdp:
+        if char.majuscule():
+            return True
+    return False
+
+def verif_chiffre(mdp):
+    for char in mdp:
+        if char.chiffre():
+            return True
+    return False
+
+def verif_caracetere_spe(mdp):
+    speciale_caracetere = "!@#\$%\^&\*"
+    for char in mdp:
+        if char in speciale_caracetere:
+            return True
+    return False
+
+def mdp_valide(mdp):
+    if verif_taille_maxilmal(mdp) and verif_minuscule(mdp) and verif_majuscule(mdp) and verif_chiffre(mdp) and verif_caracetere_spe(mdp):
+        return True
+    else:
+        return False
+
+while not mdp_valide(mdp):
+    print("J'ai dit quoi ? il manque des choses... DONC je me repete ")
+    mdp = input("Dcp je me repete 8 caractères, 1 Maj, 1 minuscule, 1 chiffre, 1 @ ")
+
+print("Hmmmm il semblerait que tu sois moins bête que ce que j'imaginais")
